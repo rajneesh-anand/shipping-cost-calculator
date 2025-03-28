c3d15cbba98135e73c7a50c012345dc1de6d454f
anand.k.rajneesh@gmail.com <anand.k.rajneesh@gmail.com>
2025-03-28 12:45:59 +0400

    miniapp set header

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ const initializeTelegramSDK = async () => {
 
     if (miniApp.ready.isAvailable()) {
       await miniApp.ready();
+      miniApp.setHeaderColor("#fcb69f");
       console.log("Mini App готово");
     }
   } catch (error) {
@@ -19,38 +20,38 @@ const initializeTelegramSDK = async () => {
 };
 
 initializeTelegramSDK();
-miniApp.setHeaderColor("#fcb69f");
+// miniApp.setHeaderColor("#fcb69f");
 // Монтируем главную кнопку
-if (mainButton.mount.isAvailable()) {
-  mainButton.mount(); // Убедимся, что кнопка установлена
-  console.log("Главная кнопка установлена");
-}
+// if (mainButton.mount.isAvailable()) {
+//   mainButton.mount(); // Убедимся, что кнопка установлена
+//   console.log("Главная кнопка установлена");
+// }
 
-// Настраиваем свойства главной кнопки
-if (mainButton.setParams.isAvailable()) {
-  mainButton.setParams({
-    backgroundColor: "#aa1388", // Цвет кнопки
-    isEnabled: true, // Кнопка активна
-    isVisible: true, // Кнопка видима
-    text: "Поделиться очками", // Текст на кнопке
-    textColor: "#000000", // Цвет текста
-  });
-  console.log("Свойства главной кнопки настроены");
-}
+// // Настраиваем свойства главной кнопки
+// if (mainButton.setParams.isAvailable()) {
+//   mainButton.setParams({
+//     backgroundColor: "#aa1388", // Цвет кнопки
+//     isEnabled: true, // Кнопка активна
+//     isVisible: true, // Кнопка видима
+//     text: "Поделиться очками", // Текст на кнопке
+//     textColor: "#000000", // Цвет текста
+//   });
+//   console.log("Свойства главной кнопки настроены");
+// }
 
-// Добавляем слушатель кликов на кнопку
-if (mainButton.onClick.isAvailable()) {
-  mainButton.on("click", () => {
-    try {
-      // Получение текущих очков из localStorage
-      const score = localStorage.getItem("memory-game-score") || 0;
-      shareURL(`Посмотрите! У меня ${score} очков в игре!`);
-      console.log("Окно выбора чата открыто для отправки сообщения.");
-    } catch (error) {
-      console.error("Ошибка при открытии окна выбора чата:", error);
-    }
-  });
-}
+// // Добавляем слушатель кликов на кнопку
+// if (mainButton.onClick.isAvailable()) {
+//   mainButton.on("click", () => {
+//     try {
+//       // Получение текущих очков из localStorage
+//       const score = localStorage.getItem("memory-game-score") || 0;
+//       shareURL(`Посмотрите! У меня ${score} очков в игре!`);
+//       console.log("Окно выбора чата открыто для отправки сообщения.");
+//     } catch (error) {
+//       console.error("Ошибка при открытии окна выбора чата:", error);
+//     }
+//   });
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
